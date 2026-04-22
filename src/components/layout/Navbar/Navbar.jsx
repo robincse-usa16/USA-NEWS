19716ce600071349402d7745099a1021983e4d1e
robincse-usa16 <robincse.usa16@gmail.com>
2026-04-22 19:05:28 +0600

    all upto date. navbar fiexed

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -3,32 +3,34 @@ import Container from "../../common/Container/Container";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
+    <header className="navbar">
       <Container>
-        <div className="nav-grid">
+        <div className="nav-wrapper">
           {/* LEFT */}
           <div className="nav-left">
-            <div className="logo">USA NEWS</div>
+            <h2 className="logo">USA NEWS</h2>
           </div>
 
           {/* CENTER */}
           <div className="nav-center">
-            <input placeholder="Search the web" />
+            <div className="search-box">
+              <span>🔍</span>
+              <input placeholder="Search the web" />
+            </div>
           </div>
 
           {/* RIGHT */}
           <div className="nav-right">
-            <span>🌤 35°C</span>
-            <button className="icon-btn">⚙</button>
+            <div className="weather">🌤 35°C</div>
             <button className="signin">Sign in</button>
           </div>
         </div>
       </Container>
 
-      {/* CATEGORY BAR */}
-      <div className="category-bar">
+      {/* CATEGORY */}
+      <div className="nav-bottom">
         <Container>
-          <div className="category-container">
+          <div className="categories">
             <span className="active">Discover</span>
             <span>Sports</span>
             <span>Play</span>
@@ -38,6 +40,6 @@ export default function Navbar() {
           </div>
         </Container>
       </div>
-    </div>
+    </header>
   );
 }
